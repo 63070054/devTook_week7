68c5885fde0b37e0575e7185f819cab534e3918e
ntp_vv <73682760+63070054@users.noreply.github.com>
2023-03-03 19:42:46 +0700

    Add files via upload

---
+++ myscript.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fullName", help="the fullName to greet")
+parser.add_argument("height", help="the height of the person being greeted", type=int)
+parser.add_argument("weight", help="the weight of the person being greeted", type=int)
+parser.add_argument("age", help="the age of the person being greeted", type=int)
+args = parser.parse_args()
+
+print("Your FullName is " + str(args.fullName))
+print("Your Height is " + str(args.height))
+print("Your Weight is " + str(args.weight))
+print("Your Age is " + str(args.age))
