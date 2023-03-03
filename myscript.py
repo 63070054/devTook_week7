6e9e10b42eec3944bb10322fa064756072c62807
ntp_vv <73682760+63070054@users.noreply.github.com>
2023-03-03 19:57:46 +0700

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,13 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("fullName", help="the fullName to greet")
-parser.add_argument("height", help="the height of the person being greeted", type=float)
-parser.add_argument("weight", help="the weight of the person being greeted", type=float)
+parser.add_argument("firstName", help="the firstName to greet")
+parser.add_argument("height", help="the height of the person being greeted", type=int)
+parser.add_argument("weight", help="the weight of the person being greeted", type=int)
 parser.add_argument("age", help="the age of the person being greeted", type=int)
 args = parser.parse_args()
 
-print("Your FullName is " + str(args.fullName))
+print("Your FirstName is " + str(args.firstName))
 print("Your Height is " + str(args.height))
 print("Your Weight is " + str(args.weight))
 print("Your Age is " + str(args.age))
